1a18b67abe26212324fab2349c46f33518f3ffd5
Muri-tuu <kenhopkins001@gmail.com>
2024-07-29 00:45:51 +0300

    ALLTECH E-COMMERCE WEBSITE

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 import classes from './index.module.scss'
 
@@ -11,8 +11,11 @@ const Promotion = () => {
     seconds: 0,
   })
 
-  const targetDate = new Date();
-  targetDate.setDate(targetDate.getDate() + 3)
+  const targetDate = useMemo(() => {
+    const date = new Date()
+    date.setDate(date.getDate() + 3)
+    return date
+  }, [])
 
   useEffect(() => {
     const timerInterval = setInterval(() => {
@@ -33,8 +36,8 @@ const Promotion = () => {
     }, 1000)
 
     return () => {
-      clearInterval(timerInterval); // Cleanup the interval when the component unmounts.
-    };
+      clearInterval(timerInterval) // Cleanup the interval when the component unmounts.
+    }
   }, [targetDate])
 
   return (
